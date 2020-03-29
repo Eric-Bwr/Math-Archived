d932b61907b23a99dc3180e4b7a4e5e34f79f70f
Eric-Bwr <boewer2003@gmail.com>
2020-03-29 19:36:03 +0200

    Include fix

---
+++ Build/include/Util/Random.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+void setSeed(unsigned int seed);
+int getRandomInt(int minNum, int maxNum);
+float getRandomFloat(float minNum, float maxNum);
+uint64_t getRandomSeed();
