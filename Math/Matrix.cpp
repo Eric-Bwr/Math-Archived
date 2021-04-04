314bf4e8ce0158a2a04956106f1941f90ca15093
Eric-Bwr <boewer2003@gmail.com>
2021-04-04 15:36:27 +0200

    Do not translate the m33

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -27,7 +27,6 @@ Mat4f &Mat4f::translate(float x, float y, float z) {
     m30 = m00 * x + m10 * y + m20 * z;
     m31 = m01 * x + m11 * y + m21 * z;
     m32 = m02 * x + m12 * y + m22 * z;
-    m33 = m03 * x + m13 * y + m23 * z;
 
     updateBuffer();
     return *this;
