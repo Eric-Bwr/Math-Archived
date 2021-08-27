90417e5872a6674f336a989e9f51eabcd2f5a5fe
Eric-Bwr <boewer2003@gmail.com>
2021-08-27 17:51:18 +0200

    Templates

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -2,14 +2,16 @@
 
 #define RADIANS 0.01745329251994329576923690768489
 
-Mat4::Mat4(double v) {
+template<typename T>
+Mat4<T>::Mat4(T v) {
 	m00 = v;
 	m11 = v;
 	m22 = v;
 	m33 = v;
 }
 
-Mat4 &Mat4::identity() {
+template<typename T>
+Mat4<T> &Mat4<T>::identity() {
     m00 = 1.0;
     m11 = 1.0;
     m22 = 1.0;
@@ -17,11 +19,13 @@ Mat4 &Mat4::identity() {
     return *this;
 }
 
-Mat4 &Mat4::translate(const Vec3 &v) {
+template<typename T>
+Mat4<T> &Mat4<T>::translate(const Vec3<T> &v) {
     return translate(v.x, v.y, v.z);
 }
 
-Mat4 &Mat4::translate(double x, double y, double z) {
+template<typename T>
+Mat4<T> &Mat4<T>::translate(T x, T y, T z) {
     m30 = m00 * x + m10 * y + m20 * z;
     m31 = m01 * x + m11 * y + m21 * z;
     m32 = m02 * x + m12 * y + m22 * z;
@@ -29,42 +33,44 @@ Mat4 &Mat4::translate(double x, double y, double z) {
     return *this;
 }
 
-Mat4 &Mat4::rotate(double angle, const Vec3 &axis) {
+template<typename T>
+Mat4<T> &Mat4<T>::rotate(T angle, const Vec3<T> &axis) {
 	return rotate(angle, axis.x, axis.y, axis.z);
 }
 
-Mat4 &Mat4::rotate(double angle, double x, double y, double z) {
-	double angleRadians = angle * RADIANS;
-	double c = std::cos(angleRadians);
-	double s = std::sin(angleRadians);
-	double oneminusc = 1.0 - c;
-	double xy = x * y;
-	double yz = y * z;
-	double xz = x * z;
-	double xs = x * s;
-	double ys = y * s;
-	double zs = z * s;
-	double f00 = x * x * oneminusc + c;
-	double f2 = xy * oneminusc + zs;
-	double f3 = xz * oneminusc - ys;
-	double f4 = xy * oneminusc - zs;
-	double f5 = y * y * oneminusc + c;
-	double f6 = yz * oneminusc + xs;
-	double f7 = xz * oneminusc + ys;
-	double f8 = yz * oneminusc - xs;
-	double f9 = z * z * oneminusc + c;
-	double e1 = m00 * f00 + m10 * f2 + m20 * f3;
-	double e2 = m01 * f00 + m11 * f2 + m21 * f3;
-	double e3 = m02 * f00 + m12 * f2 + m22 * f3;
-	double e4 = m03 * f00 + m13 * f2 + m23 * f3;
-	double e5 = m00 * f4 + m10 * f5 + m20 * f6;
-	double e6 = m01 * f4 + m11 * f5 + m21 * f6;
-	double e7 = m02 * f4 + m12 * f5 + m22 * f6;
-	double e8 = m03 * f4 + m13 * f5 + m23 * f6;
-	double e9 = m00 * f7 + m10 * f8 + m20 * f9;
-	double e10 = m01 * f7 + m11 * f8 + m21 * f9;
-	double e11 = m02 * f7 + m12 * f8 + m22 * f9;
-	double e12 = m03 * f7 + m13 * f8 + m23 * f9;
+template<typename T>
+Mat4<T> &Mat4<T>::rotate(T angle, T x, T y, T z) {
+	T angleRadians = angle * RADIANS;
+	T c = std::cos(angleRadians);
+	T s = std::sin(angleRadians);
+	T oneminusc = 1.0 - c;
+	T xy = x * y;
+	T yz = y * z;
+	T xz = x * z;
+	T xs = x * s;
+	T ys = y * s;
+	T zs = z * s;
+	T f00 = x * x * oneminusc + c;
+	T f2 = xy * oneminusc + zs;
+	T f3 = xz * oneminusc - ys;
+	T f4 = xy * oneminusc - zs;
+	T f5 = y * y * oneminusc + c;
+	T f6 = yz * oneminusc + xs;
+	T f7 = xz * oneminusc + ys;
+	T f8 = yz * oneminusc - xs;
+	T f9 = z * z * oneminusc + c;
+	T e1 = m00 * f00 + m10 * f2 + m20 * f3;
+	T e2 = m01 * f00 + m11 * f2 + m21 * f3;
+	T e3 = m02 * f00 + m12 * f2 + m22 * f3;
+	T e4 = m03 * f00 + m13 * f2 + m23 * f3;
+	T e5 = m00 * f4 + m10 * f5 + m20 * f6;
+	T e6 = m01 * f4 + m11 * f5 + m21 * f6;
+	T e7 = m02 * f4 + m12 * f5 + m22 * f6;
+	T e8 = m03 * f4 + m13 * f5 + m23 * f6;
+	T e9 = m00 * f7 + m10 * f8 + m20 * f9;
+	T e10 = m01 * f7 + m11 * f8 + m21 * f9;
+	T e11 = m02 * f7 + m12 * f8 + m22 * f9;
+	T e12 = m03 * f7 + m13 * f8 + m23 * f9;
 	m00 = e1;
 	m01 = e2;
 	m02 = e3;
@@ -80,11 +86,13 @@ Mat4 &Mat4::rotate(double angle, double x, double y, double z) {
 	return *this;
 }
 
-Mat4 &Mat4::scale(double s) {
+template<typename T>
+Mat4<T> &Mat4<T>::scale(T s) {
 	return scale(s, s, s);
 }
 
-Mat4 &Mat4::scale(double x, double y, double z) {
+template<typename T>
+Mat4<T> &Mat4<T>::scale(T x, T y, T z) {
 	m00 *= x;
 	m01 *= x;
 	m02 *= x;
@@ -100,11 +108,13 @@ Mat4 &Mat4::scale(double x, double y, double z) {
 	return *this;
 }
 
-Mat4 &Mat4::scale(const Vec3 &v) {
+template<typename T>
+Mat4<T> &Mat4<T>::scale(const Vec3<T> &v) {
     return scale(v.x, v.y, v.z);
 }
 
-Mat4& Mat4::multiply(const Mat4& matrix) {
+template<typename T>
+Mat4<T>& Mat4<T>::multiply(const Mat4<T>& matrix) {
     m00 = m00 * matrix.m00 + m10 * matrix.m01 + m20 * matrix.m02 + m30 * matrix.m03;
     m01 = m01 * matrix.m00 + m11 * matrix.m01 + m21 * matrix.m02 + m31 * matrix.m03;
     m02 = m02 * matrix.m00 + m12 * matrix.m01 + m22 * matrix.m02 + m32 * matrix.m03;
@@ -125,7 +135,8 @@ Mat4& Mat4::multiply(const Mat4& matrix) {
     return *this;
 }
 
-Mat4& Mat4::multiply(const Mat4& one, const Mat4& two) {
+template<typename T>
+Mat4<T>& Mat4<T>::multiply(const Mat4<T>& one, const Mat4<T>& two) {
     m00 = one.m00 * two.m00 + one.m10 * two.m01 + one.m20 * two.m02 + one.m30 * two.m03;
     m01 = one.m01 * two.m00 + one.m11 * two.m01 + one.m21 * two.m02 + one.m31 * two.m03;
     m02 = one.m02 * two.m00 + one.m12 * two.m01 + one.m22 * two.m02 + one.m32 * two.m03;
@@ -146,14 +157,16 @@ Mat4& Mat4::multiply(const Mat4& one, const Mat4& two) {
     return *this;
 }
 
-double determinant3x3(double t00, double t01, double t02, double t10, double t11, double t12, double t20, double t21, double t22) {
+template<typename T>
+T determinant3x3(T t00, T t01, T t02, T t10, T t11, T t12, T t20, T t21, T t22) {
     return   t00 * (t11 * t22 - t12 * t21)
              + t01 * (t12 * t20 - t10 * t22)
              + t02 * (t10 * t21 - t11 * t20);
 }
 
-Mat4& Mat4::invert() {
-    double f = m00 * ((m11 * m22 * m33 + m12 * m23 * m31 + m13 * m21 * m32)
+template<typename T>
+Mat4<T>& Mat4<T>::invert() {
+    T f = m00 * ((m11 * m22 * m33 + m12 * m23 * m31 + m13 * m21 * m32)
                      - m13 * m22 * m31- m11 * m23 * m32- m12 * m21 * m33);
     f -= m01 * ((m10 * m22 * m33 + m12 * m23 * m30 + m13 * m20 * m32)
                 - m13 * m22 * m30- m10 * m23 * m32- m12 * m20 * m33);
@@ -161,27 +174,27 @@ Mat4& Mat4::invert() {
                 - m13 * m21 * m30- m10 * m23 * m31- m11 * m20 * m33);
     f -= m03 * ((m10 * m21 * m32 + m11 * m22 * m30 + m12 * m20 * m31)
                 - m12 * m21 * m30- m10 * m22 * m31- m11 * m20 * m32);
-    double determinant_inv = 1.0 / f;
+    T determinant_inv = 1.0 / f;
 
-    double t00 =  determinant3x3(m11, m12, m13, m21, m22, m23, m31, m32, m33);
-    double t01 = -determinant3x3(m10, m12, m13, m20, m22, m23, m30, m32, m33);
-    double t02 =  determinant3x3(m10, m11, m13, m20, m21, m23, m30, m31, m33);
-    double t03 = -determinant3x3(m10, m11, m12, m20, m21, m22, m30, m31, m32);
+    T t00 =  determinant3x3(m11, m12, m13, m21, m22, m23, m31, m32, m33);
+    T t01 = -determinant3x3(m10, m12, m13, m20, m22, m23, m30, m32, m33);
+    T t02 =  determinant3x3(m10, m11, m13, m20, m21, m23, m30, m31, m33);
+    T t03 = -determinant3x3(m10, m11, m12, m20, m21, m22, m30, m31, m32);
 
-    double t10 = -determinant3x3(m01, m02, m03, m21, m22, m23, m31, m32, m33);
-    double t11 =  determinant3x3(m00, m02, m03, m20, m22, m23, m30, m32, m33);
-    double t12 = -determinant3x3(m00, m01, m03, m20, m21, m23, m30, m31, m33);
-    double t13 =  determinant3x3(m00, m01, m02, m20, m21, m22, m30, m31, m32);
+    T t10 = -determinant3x3(m01, m02, m03, m21, m22, m23, m31, m32, m33);
+    T t11 =  determinant3x3(m00, m02, m03, m20, m22, m23, m30, m32, m33);
+    T t12 = -determinant3x3(m00, m01, m03, m20, m21, m23, m30, m31, m33);
+    T t13 =  determinant3x3(m00, m01, m02, m20, m21, m22, m30, m31, m32);
 
-    double t20 =  determinant3x3(m01, m02, m03, m11, m12, m13, m31, m32, m33);
-    double t21 = -determinant3x3(m00, m02, m03, m10, m12, m13, m30, m32, m33);
-    double t22 =  determinant3x3(m00, m01, m03, m10, m11, m13, m30, m31, m33);
-    double t23 = -determinant3x3(m00, m01, m02, m10, m11, m12, m30, m31, m32);
+    T t20 =  determinant3x3(m01, m02, m03, m11, m12, m13, m31, m32, m33);
+    T t21 = -determinant3x3(m00, m02, m03, m10, m12, m13, m30, m32, m33);
+    T t22 =  determinant3x3(m00, m01, m03, m10, m11, m13, m30, m31, m33);
+    T t23 = -determinant3x3(m00, m01, m02, m10, m11, m12, m30, m31, m32);
 
-    double t30 = -determinant3x3(m01, m02, m03, m11, m12, m13, m21, m22, m23);
-    double t31 =  determinant3x3(m00, m02, m03, m10, m12, m13, m20, m22, m23);
-    double t32 = -determinant3x3(m00, m01, m03, m10, m11, m13, m20, m21, m23);
-    double t33 =  determinant3x3(m00, m01, m02, m10, m11, m12, m20, m21, m22);
+    T t30 = -determinant3x3(m01, m02, m03, m11, m12, m13, m21, m22, m23);
+    T t31 =  determinant3x3(m00, m02, m03, m10, m12, m13, m20, m22, m23);
+    T t32 = -determinant3x3(m00, m01, m03, m10, m11, m13, m20, m21, m23);
+    T t33 =  determinant3x3(m00, m01, m02, m10, m11, m12, m20, m21, m22);
 
     m00 = t00 * determinant_inv;
     m11 = t11 * determinant_inv;
@@ -203,14 +216,16 @@ Mat4& Mat4::invert() {
     return *this;
 }
 
-Mat4 &Mat4::removeTranslation() {
+template<typename T>
+Mat4<T> &Mat4<T>::removeTranslation() {
     m30 = 0.0;
     m31 = 0.0;
     m32 = 0.0;
     return *this;
 }
 
-Mat4 &Mat4::orthographic(double left, double right, double bottom, double top, double near, double far){
+template<typename T>
+Mat4<T> &Mat4<T>::orthographic(T left, T right, T bottom, T top, T near, T far){
     m00 = 2.0 / (right - left);
     m11 = 2.0 / (top - bottom);
     m22 = 2.0 / (near - far);
@@ -222,9 +237,10 @@ Mat4 &Mat4::orthographic(double left, double right, double bottom, double top, d
     return *this;
 }
 
-Mat4 &Mat4::perspective(double fov, double width, double height, double near, double far){
-    double aspectRatio = width / height;
-    double yScale = std::tan(fov / 2.0 * RADIANS);
+template<typename T>
+Mat4<T> &Mat4<T>::perspective(T fov, T width, T height, T near, T far){
+    T aspectRatio = width / height;
+    T yScale = std::tan(fov / 2.0 * RADIANS);
 
     m00 = 1.0 / (aspectRatio * yScale);
     m11 = 1.0 / (yScale);
@@ -235,7 +251,8 @@ Mat4 &Mat4::perspective(double fov, double width, double height, double near, do
     return *this;
 }
 
-Mat4 &Mat4::lookAt(const Vec3 &eye, const Vec3 &center, const Vec3 &up) {
+template<typename T>
+Mat4<T> &Mat4<T>::lookAt(const Vec3<T> &eye, const Vec3<T> &center, const Vec3<T> &up) {
     Vec3 f = (center - eye).norm();
     Vec3 s = (f.cross(up)).norm();
     Vec3 u = s.cross(f);
@@ -256,31 +273,34 @@ Mat4 &Mat4::lookAt(const Vec3 &eye, const Vec3 &center, const Vec3 &up) {
     return *this;
 }
 
-char *Mat4::toString() const {
+template<typename T>
+char *Mat4<T>::toString() const {
 	const char *mt = "|%+6.2f %+6.2f %+6.2f %+6.2f|\n|%+6.2f %+6.2f %+6.2f %+6.2f|\n|%+6.2f %+6.2f %+6.2f %+6.2f|\n|%+6.2f %+6.2f %+6.2f %+6.2f|\n";
     char *s = (char *) malloc(sizeof(char) * 128);
 	std::sprintf(s, mt, m00, m10, m20, m30, m01, m11, m21, m31, m02, m12, m22, m32, m03, m13, m23, m33);
 	return s;
 }
 
-const double* Mat4::getBuffer() const {
+template<typename T>
+const T* Mat4<T>::getBuffer() const {
     return &m00;
 }
 
-double* Mat4::getBuffer() {
+template<typename T>
+T* Mat4<T>::getBuffer() {
     return &m00;
 }
 
-Mat4::~Mat4() = default;
-
-Mat4 identityMatrix() {
-	return Mat4(1.0);
+template<typename T>
+Mat4<T> identityMatrix() {
+	return Mat4<T>(1.0);
 }
 
-Mat4 perspectiveMatrix(double fov, double width, double height, double near, double far) {
-	Mat4 mat = identityMatrix();
-	double aspectRatio = width / height;
-	double yScale = std::tan(fov / 2.0 * RADIANS);
+template<typename T>
+Mat4<T> perspectiveMatrix(T fov, T width, T height, T near, T far) {
+	Mat4<T> mat = identityMatrix();
+	T aspectRatio = width / height;
+	T yScale = std::tan(fov / 2.0 * RADIANS);
 
 	mat.m00 = 1.0 / (aspectRatio * yScale);
 	mat.m11 = 1.0 / (yScale);
@@ -291,8 +311,9 @@ Mat4 perspectiveMatrix(double fov, double width, double height, double near, dou
 	return mat;
 }
 
-Mat4 orthographicMatrix(double left, double right, double bottom, double top, double near, double far) {
-	Mat4 mat = identityMatrix();
+template<typename T>
+Mat4<T> orthographicMatrix(T left, T right, T bottom, T top, T near, T far) {
+	Mat4<T> mat = identityMatrix();
 
 	mat.m00 = 2.0 / (right - left);
 	mat.m11 = 2.0 / (top - bottom);
@@ -305,8 +326,9 @@ Mat4 orthographicMatrix(double left, double right, double bottom, double top, do
 	return mat;
 }
 
-Mat4 multiplyMatrix(const Mat4& one, const Mat4& two) {
-	Mat4 mat = one;
+template<typename T>
+Mat4<T> multiplyMatrix(const Mat4<T>& one, const Mat4<T>& two) {
+	Mat4<T> mat = one;
 
     mat.m00 = mat.m00 * two.m00 + mat.m10 * two.m01 + mat.m20 * two.m02 + mat.m30 * two.m03;
     mat.m01 = mat.m01 * two.m00 + mat.m11 * two.m01 + mat.m21 * two.m02 + mat.m31 * two.m03;
@@ -328,8 +350,9 @@ Mat4 multiplyMatrix(const Mat4& one, const Mat4& two) {
 	return mat;
 }
 
-Mat4 lookAtMatrix(const Vec3 &eye, const Vec3 &center, const Vec3 &up) {
-	Mat4 mat = identityMatrix();
+template<typename T>
+Mat4<T> lookAtMatrix(const Vec3<T> &eye, const Vec3<T> &center, const Vec3<T> &up) {
+	Mat4<T> mat = identityMatrix();
 	Vec3 f = (center - eye).norm();
 	Vec3 s = (f.cross(up)).norm();
 	Vec3 u = s.cross(f);
--- Math/Vector.cpp
+++ Math/Vector.cpp
@@ -1,266 +1,322 @@
 #include "Vector.h"
 
-Vec2::Vec2(double v) {
+template<typename T>
+Vec2<T>::Vec2(T v) {
 	this->x = v;
 	this->y = v;
 }
 
-Vec2::Vec2(double x, double y) {
+template<typename T>
+Vec2<T>::Vec2(T x, T y) {
 	this->x = x;
 	this->y = y;
 }
 
-Vec2::Vec2(const Vec2 &v) {
+template<typename T>
+Vec2<T>::Vec2(const Vec2<T> &v) {
 	this->x = v.x;
 	this->y = v.y;
 }
 
-Vec2 operator+ (const Vec2 &a, const Vec2 &b) {
-	return Vec2(a.x + b.x, a.y + b.y);
+template<typename T>
+Vec2<T> operator+ (const Vec2<T> &a, const Vec2<T> &b) {
+	return Vec2<T>(a.x + b.x, a.y + b.y);
 }
 
-Vec2 operator+ (const Vec2 &a, double b) {
-	return Vec2(a.x + b, a.y + b);
+template<typename T>
+Vec2<T> operator+ (const Vec2<T> &a, T b) {
+	return Vec2<T>(a.x + b, a.y + b);
 }
 
-Vec2 operator- (const Vec2 &a, const Vec2 &b) {
-	return Vec2(a.x - b.x, a.y - b.y);
+template<typename T>
+Vec2<T> operator- (const Vec2<T> &a, const Vec2<T> &b) {
+	return Vec2<T>(a.x - b.x, a.y - b.y);
 }
 
-Vec2 operator- (const Vec2 &a, double b) {
-	return Vec2(a.x - b, a.y - b);
+template<typename T>
+Vec2<T> operator- (const Vec2<T> &a, T b) {
+	return Vec2<T>(a.x - b, a.y - b);
 }
 
-Vec2 operator* (const Vec2 &a, const Vec2 &b) {
-	return Vec2(a.x * b.x, a.y * b.y);
+template<typename T>
+Vec2<T> operator* (const Vec2<T> &a, const Vec2<T> &b) {
+	return Vec2<T>(a.x * b.x, a.y * b.y);
 }
 
-Vec2 operator* (const Vec2 &a, double b) {
-	return Vec2(a.x * b, a.y * b);
+template<typename T>
+Vec2<T> operator* (const Vec2<T> &a, T b) {
+	return Vec2<T>(a.x * b, a.y * b);
 }
 
-Vec2 operator/ (const Vec2 &a, const Vec2 &b) {
-	return Vec2(a.x / b.x, a.y / b.y);
+template<typename T>
+Vec2<T> operator/ (const Vec2<T> &a, const Vec2<T> &b) {
+	return Vec2<T>(a.x / b.x, a.y / b.y);
 }
 
-Vec2 operator/ (const Vec2 &a, double b) {
-	return Vec2(a.x / b, a.y / b);
+template<typename T>
+Vec2<T> operator/ (const Vec2<T> &a, T b) {
+	return Vec2<T>(a.x / b, a.y / b);
 }
 
-Vec2& Vec2::operator+= (const Vec2 &a) {
+template<typename T>
+Vec2<T>& Vec2<T>::operator+= (const Vec2<T> &a) {
 	x += a.x;
 	y += a.y;
 	return *this;
 }
 
-Vec2& Vec2::operator-= (const Vec2 &a) {
+template<typename T>
+Vec2<T>& Vec2<T>::operator-= (const Vec2<T> &a) {
 	x -= a.x;
 	y -= a.y;
 	return *this;
 }
 
-Vec2& Vec2::operator*= (double v) {
+template<typename T>
+Vec2<T>& Vec2<T>::operator*= (T v) {
 	x *= v;
 	y *= v;
 	return *this;
 }
-	
-bool Vec2::operator== (Vec2 &a) const {
+
+template<typename T>
+bool Vec2<T>::operator== (Vec2<T> &a) const {
 	return x == a.x && y == a.y;
 }
 
-bool Vec2::operator!= (Vec2 &a) const {
+template<typename T>
+bool Vec2<T>::operator!= (Vec2<T> &a) const {
 	return x != a.x || y != a.y;
 }
 
-Vec2 Vec2::norm() const {
-	double f = 1.0 / len();
-	return Vec2(f * x, f * y);
+template<typename T>
+Vec2<T> Vec2<T>::norm() const {
+	T f = 1.0 / len();
+	return Vec2<T>(f * x, f * y);
 }
 
-double Vec2::dot(Vec2 const &a) const {
+template<typename T>
+T Vec2<T>::dot(Vec2<T> const &a) const {
 	return x * a.x + y * a.y;
 }
 
-double Vec2::len() const {
+template<typename T>
+T Vec2<T>::len() const {
 	return std::sqrt(x * x + y * y);
 }
 
-double Vec2::len2() const {
+template<typename T>
+T Vec2<T>::len2() const {
 	return x * x + y * y;
 }
 
-Vec3::Vec3(double v) {
+template<typename T>
+Vec3<T>::Vec3(T v) {
 	x = v;
 	y = v;
 	z = v;
 }
 
-Vec3::Vec3(double x, double y, double z) {
+template<typename T>
+Vec3<T>::Vec3(T x, T y, T z) {
 	this->x = x;
 	this->y = y;
 	this->z = z;
 }
 
-Vec3::Vec3(const Vec2& xy, double z) {
+template<typename T>
+Vec3<T>::Vec3(const Vec2<T>& xy, T z) {
 	this->x = xy.x;
 	this->y = xy.y;
 	this->z = z;
 }
 
-Vec3::Vec3(const Vec3& v) {
+template<typename T>
+Vec3<T>::Vec3(const Vec3<T>& v) {
 	x = v.x;
 	y = v.y;
 	z = v.z;
 }
 
-Vec2 Vec3::xy() const {
-	return Vec2(x, y);
+template<typename T>
+Vec2<T> Vec3<T>::xy() const {
+	return Vec2<T>(x, y);
 }
 
-Vec2 Vec3::xz() const {
-	return Vec2(x, z);
+template<typename T>
+Vec2<T> Vec3<T>::xz() const {
+	return Vec2<T>(x, z);
 }
 
-Vec2 Vec3::yz() const {
-	return Vec2(y, z);
+template<typename T>
+Vec2<T> Vec3<T>::yz() const {
+	return Vec2<T>(y, z);
 }
 
-Vec3 operator+ (const Vec3 &a, const Vec3 &b) {
-	return Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+template<typename T>
+Vec3<T> operator+ (const Vec3<T> &a, const Vec3<T> &b) {
+	return Vec3<T>(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-Vec3 operator+ (const Vec3 &a, double b) {
-	return Vec3(a.x + b, a.y + b, a.z + b);
+template<typename T>
+Vec3<T> operator+ (const Vec3<T> &a, T b) {
+	return Vec3<T>(a.x + b, a.y + b, a.z + b);
 }
 
-Vec3 operator- (const Vec3 &a, const Vec3 &b) {
-	return Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+template<typename T>
+Vec3<T> operator- (const Vec3<T> &a, const Vec3<T> &b) {
+	return Vec3<T>(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
-Vec3 operator- (const Vec3 &a, double b) {
-	return Vec3(a.x - b, a.y - b, a.z - b);
+template<typename T>
+Vec3<T> operator- (const Vec3<T> &a, T b) {
+	return Vec3<T>(a.x - b, a.y - b, a.z - b);
 }
 
-Vec3 operator* (const Vec3 &a, const Vec3 &b) {
-	return Vec3(a.x * b.x, a.y * b.y, a.z * b.z);
+template<typename T>
+Vec3<T> operator* (const Vec3<T> &a, const Vec3<T> &b) {
+	return Vec3<T>(a.x * b.x, a.y * b.y, a.z * b.z);
 }
 
-Vec3 operator* (const Vec3 &a, double b) {
-	return Vec3(a.x * b, a.y * b, a.z * b);
+template<typename T>
+Vec3<T> operator* (const Vec3<T> &a, T b) {
+	return Vec3<T>(a.x * b, a.y * b, a.z * b);
 }
 
-Vec3 operator/ (const Vec3 &a, const Vec3 &b) {
-	return Vec3(a.x / b.x, a.y / b.y, a.z / b.z);
+template<typename T>
+Vec3<T> operator/ (const Vec3<T> &a, const Vec3<T> &b) {
+	return Vec3<T>(a.x / b.x, a.y / b.y, a.z / b.z);
 }
 
-Vec3 operator/ (const Vec3 &a, double b) {
-	return Vec3(a.x / b, a.y / b, a.z / b);
+template<typename T>
+Vec3<T> operator/ (const Vec3<T> &a, T b) {
+	return Vec3<T>(a.x / b, a.y / b, a.z / b);
 }
 
-Vec3& Vec3::operator+= (const Vec3 &a) {
+template<typename T>
+Vec3<T>& Vec3<T>::operator+= (const Vec3<T> &a) {
 	x += a.x;
 	y += a.y;
 	z += a.z;
 	return *this;
 }
 
-Vec3& Vec3::operator-= (const Vec3 &a) {
+template<typename T>
+Vec3<T>& Vec3<T>::operator-= (const Vec3<T> &a) {
 	x -= a.x;
 	y -= a.y;
 	z -= a.z;
 	return *this;
 }
 
-Vec3& Vec3::operator*= (double v) {
+template<typename T>
+Vec3<T>& Vec3<T>::operator*= (T v) {
 	x *= v;
 	y *= v;
 	z *= v;
 	return *this;
 }
-	
-bool Vec3::operator== (Vec3 &a) const {
+
+template<typename T>
+bool Vec3<T>::operator== (Vec3<T> &a) const {
 	return x == a.x && y == a.y && z == a.z;
 }
 
-bool Vec3::operator!= (Vec3 &a) const {
+template<typename T>
+bool Vec3<T>::operator!= (Vec3<T> &a) const {
 	return x != a.x || y != a.y || z != a.z;
 }
 
-Vec3 Vec3::cross(Vec3 const &a) const {
-	double xx = y * a.z - a.y * z;
-    double yy = z * a.x - a.z * x;
-    double zz = x * a.y - a.x * y;
-	return Vec3(xx, yy, zz);
+template<typename T>
+Vec3<T> Vec3<T>::cross(Vec3<T> const &a) const {
+	T xx = y * a.z - a.y * z;
+    T yy = z * a.x - a.z * x;
+    T zz = x * a.y - a.x * y;
+	return Vec3<T>(xx, yy, zz);
 }
 
-Vec3 Vec3::norm() const {
-    double lengthSquared = (x * x + y * y + z * z);
+template<typename T>
+Vec3<T> Vec3<T>::norm() const {
+    T lengthSquared = (x * x + y * y + z * z);
     if(lengthSquared == 0)
         return *this;
-    double length = 1.0 / std::sqrt(lengthSquared);
-	return Vec3(x * length, y * length, z * length);
+    T length = 1.0 / std::sqrt(lengthSquared);
+	return Vec3<T>(x * length, y * length, z * length);
 }
 
-double Vec3::dot(Vec3 const &a) const {
+template<typename T>
+T Vec3<T>::dot(Vec3<T> const &a) const {
 	return x * a.x + y * a.y + z * a.z;
 }
 
-double Vec3::len() const {
+template<typename T>
+T Vec3<T>::len() const {
 	return std::sqrt(x * x + y * y + z * z);
 }
 
-double Vec3::len2() const {
+template<typename T>
+T Vec3<T>::len2() const {
 	return x * x + y * y + z * z;
 }
 
-Vec4::Vec4(double v) {
+template<typename T>
+Vec4<T>::Vec4(T v) {
     x = v;
     y = v;
     z = v;
     w = v;
 }
 
-Vec4::Vec4(double x, double y, double z, double w) {
+template<typename T>
+Vec4<T>::Vec4(T x, T y, T z, T w) {
     this->x = x;
     this->y = y;
     this->z = z;
     this->w = w;
 }
 
-Vec4 operator+ (const Vec4 &a, const Vec4 &b) {
-    return Vec4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+template<typename T>
+Vec4<T> operator+ (const Vec4<T> &a, const Vec4<T> &b) {
+    return Vec4<T>(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
 }
 
-Vec4 operator+ (const Vec4 &a, double b) {
-    return Vec4(a.x + b, a.y + b, a.z + b, a.w + b);
+template<typename T>
+Vec4<T> operator+ (const Vec4<T> &a, T b) {
+    return Vec4<T>(a.x + b, a.y + b, a.z + b, a.w + b);
 }
 
-Vec4 operator- (const Vec4 &a, const Vec4 &b) {
-    return Vec4(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
+template<typename T>
+Vec4<T> operator- (const Vec4<T> &a, const Vec4<T> &b) {
+    return Vec4<T>(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
 }
 
-Vec4 operator- (const Vec4 &a, double b) {
-    return Vec4(a.x - b, a.y - b, a.z - b, a.w - b);
+template<typename T>
+Vec4<T> operator- (const Vec4<T> &a, T b) {
+    return Vec4<T>(a.x - b, a.y - b, a.z - b, a.w - b);
 }
 
-Vec4 operator* (const Vec4 &a, const Vec4 &b) {
-    return Vec4(a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w);
+template<typename T>
+Vec4<T> operator* (const Vec4<T> &a, const Vec4<T> &b) {
+    return Vec4<T>(a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w);
 }
 
-Vec4 operator* (const Vec4 &a, double b) {
-    return Vec4(a.x * b, a.y * b, a.z * b, a.w * b);
+template<typename T>
+Vec4<T> operator* (const Vec4<T> &a, T b) {
+    return Vec4<T>(a.x * b, a.y * b, a.z * b, a.w * b);
 }
 
-Vec4 operator/ (const Vec4 &a, const Vec4 &b) {
-    return Vec4(a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w);
+template<typename T>
+Vec4<T> operator/ (const Vec4<T> &a, const Vec4<T> &b) {
+    return Vec4<T>(a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w);
 }
 
-Vec4 operator/ (const Vec4 &a, double b) {
-    return Vec4(a.x / b, a.y / b, a.z / b, a.w / b);
+template<typename T>
+Vec4<T> operator/ (const Vec4<T> &a, T b) {
+    return Vec4<T>(a.x / b, a.y / b, a.z / b, a.w / b);
 }
 
-Vec4& Vec4::operator+= (const Vec4 &a) {
+template<typename T>
+Vec4<T>& Vec4<T>::operator+= (const Vec4<T> &a) {
     x += a.x;
     y += a.y;
     z += a.z;
@@ -268,7 +324,8 @@ Vec4& Vec4::operator+= (const Vec4 &a) {
     return *this;
 }
 
-Vec4& Vec4::operator-= (const Vec4 &a) {
+template<typename T>
+Vec4<T>& Vec4<T>::operator-= (const Vec4<T> &a) {
     x -= a.x;
     y -= a.y;
     z -= a.z;
@@ -276,7 +333,8 @@ Vec4& Vec4::operator-= (const Vec4 &a) {
     return *this;
 }
 
-Vec4& Vec4::operator*= (double v) {
+template<typename T>
+Vec4<T>& Vec4<T>::operator*= (T v) {
     x *= v;
     y *= v;
     z *= v;
@@ -284,10 +342,12 @@ Vec4& Vec4::operator*= (double v) {
     return *this;
 }
 
-bool Vec4::operator== (Vec4 &a) const {
+template<typename T>
+bool Vec4<T>::operator== (Vec4<T> &a) const {
     return x == a.x && y == a.y && z == a.z && w == a.w;
 }
 
-bool Vec4::operator!= (Vec4 &a) const {
+template<typename T>
+bool Vec4<T>::operator!= (Vec4<T> &a) const {
     return x != a.x || y != a.y || z != a.z || w != a.w;
 }
